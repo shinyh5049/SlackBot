c2a800205851218c545123ace9266d7b02768da9
Kyeongho Jo <dagh1218@naver.com>
2018-10-25 18:18:45 +0900

    Setting List

--- education_slackbot/src/main/java/ac/knu/service/CommandParsingService.java
+++ education_slackbot/src/main/java/ac/knu/service/CommandParsingService.java
@@ -9,27 +9,42 @@ import java.util.List;
 public class CommandParsingService {
     private List<String> commandlist = new ArrayList<>();
     private List<Friends> friendslist = new ArrayList<>();
-
     public CommandParsingService() {
         commandlist.add("add");
         commandlist.add("list");
         commandlist.add("remove");
         commandlist.add("find");
+        Friends f1 = new Friends("6조", Gender.MAN, 15);
+        Friends f2 = new Friends("경호", Gender.MAN, 24);
+        friendslist.add(f1);
+        friendslist.add(f2);
 
     }
+
     public String parseCommand(String list) {
         String[] result;
         result = list.split(" ");
+        String str = "";
 
         if (result[1].equals(commandlist.get(0))) {
             return "addFriends";
         } else if (result[1].equals(commandlist.get(1))) {
-            return "friendsList";
+            if (friendslist.isEmpty()) {
+                return "친구가 없습니다.";
+            }
+            else {
+                for (int i = 0; i < friendslist.size(); i++) {
+                    str += friendslist.get(i).getName() + " " + friendslist.get(i).getGender() + " " + friendslist.get(i).getAge() + "\n";
+                }
+                return str;
+            }
         } else if (result[1].equals(commandlist.get(2))) {
-            return "removeFriends";
+            return "friendsRemove";
         } else if (result[1].equals(commandlist.get(3))) {
             return "findFriends";
         }
         return "failed";
     }
+
+
 }
--- education_slackbot/src/test/java/ac/knu/service/FriendsTest.java
+++ education_slackbot/src/test/java/ac/knu/service/FriendsTest.java
@@ -5,14 +5,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class FriendsTest {
-    Friends friends = new Friends("ds", "m", 23);
+    Friends friends = new Friends("ds", Gender.MAN, 23);
     @Test
     public void sample_test_age() {
         assertTrue(friends.getAge() == 23);
-    }
-    @Test
-    public void sample_test_sex() {
-        assertTrue(friends.getSex().equals("m"));
     }
 
     @Test
